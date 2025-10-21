eb4337fd71e5d2ff6ef7a63be98633fdf9c867c7
InventorySystemAdmin <inventory.admin@example.com>
2025-10-21 13:08:58 +0530

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/space-portfolio',
+  assetPrefix: '/space-portfolio/',
+}
 
 module.exports = nextConfig
